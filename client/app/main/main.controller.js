09d9422b67e944f3972f0db7cf810e1b1f3846df
juanCodoba <jfcordoba82@misena.edu.co>
2017-11-04 06:42:15 -0500

    animate slide page main

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,24 +2,50 @@
 
 (function() {
 
-class MainController {
+    class MainController {
 
-  constructor($http) {
-    this.$http = $http;
-    this.awesomeThings = [];
-  }
+        constructor($http) {
+            this.$http = $http;
+            this.awesomeThings = [];
+        }
 
-  $onInit() {
-    this.$http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
-  }
-}
+        $onInit() {
+            this.$http.get('/api/things').then(response => {
+                this.awesomeThings = response.data;
+            })
 
-angular.module('bootstrapAppApp')
-  .component('main', {
-    templateUrl: 'app/main/main.html',
-    controller: MainController
-  });
+            function doAnimations(elems) {
+                var animEndEv = 'webKitAnimationEnd animationed';
+                elems.each(function() {
+                    var $this = $(this),
+                        $animationType = $this.data('animation');
+                    // $this.$animationType = this.data('animation');
+                    $this.addClass($animationType).one(animEndEv, function() {
+                        $this.removeClass($animationType);
+                    });
+                });
+            }
+            var $myCarousel = $('#productosCarousel'),
+                $firstAnimatingElems = $myCarousel.find('.item:first').find(
+                    "[data-animation ^= 'animated']");
+
+            $myCarousel.carousel();
+
+            doAnimations($firstAnimatingElems);
+
+            $myCarousel.on('slide.bs.carousel', function(e) {
+                var $animatingElems = $(e.relatedTarget).find(
+                    "[data-animation ^= 'animated']");
+                doAnimations($animatingElems);
+            });
+        }
+    }
+
+    angular.module('bootstrapAppApp')
+        .component('main', {
+            templateUrl: 'app/main/main.html',
+            controller: MainController,
+            controllerAs: 'vm'
+        });
 
 })();
